02b41a54d771e47a8cf69139e71e01a9251150f1
Jaypal Solanki <jaypsl263@gmail.com>
2018-07-11 10:31:31 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="App-title">I am a React Developer</h1>
+       </div>
+    );
+  }
+}
+
+export default App;
+
+
