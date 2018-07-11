aa1c3041014927d0844a84b857e3b10a68f26b40
Jaypal Solanki <jaypsl263@gmail.com>
2018-07-11 11:38:51 -0400

    passing method references between components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons : [
+      {name : 'jay', age : 25},
+      {name : 'kay', age : 26},
+      {name : 'may', age : 29}
+    ]
+
+  }
+
+  changeNameHandler = (newName) => {
+    // console.log('clicked');
+    this.setState({
+      persons : [
+        {name : newName, age : 25},
+        {name : 'kay', age : 26},
+        {name : 'maysolanki', age : 30}
+  
+      ]
+    })
+    
+  }
+
   render() {
     return (
       <div className="App">
         <h1 className="App-title">I am a React Developer</h1>
-        <Person name="jay" age="25"/>
-        <Person name="kay" age="26"> -  I am an investment banker</Person>
-        <Person name="may" age="29"/>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person click = {this.changeNameHandler.bind(this, 'jaysolanki')} name={this.state.persons[1].name} age={this.state.persons[1].age}> -  I am an investment banker</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={(event) => this.changeNameHandler('jaysahani')}>Change Name</button>
        </div>
     );
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Person = (props) => {
     return (
         <div>
-        <p> I am {props.name} and i am {props.age} years old {props.children}</p>
+        <p onClick={props.click}> I am {props.name} and i am {props.age} years old {props.children} </p>
+        {/* <p>{props.click}</p> */}
         </div>
     )
 }
