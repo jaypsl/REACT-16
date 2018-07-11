cf4d5aaf147d971b034d1e96f3966cfe21350b90
Jaypal Solanki <jaypsl263@gmail.com>
2018-07-11 10:52:25 -0400

    added a functional(stateless) component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1 className="App-title">I am a React Developer</h1>
+        <Person name="jay" age="25"/>
+        <Person name="kay" age="26"> -  I am an investment banker</Person>
+        <Person name="may" age="29"/>
        </div>
     );
+
+    // return React.createElement('div', {className : 'App'} , React.createElement('h1', null, 'i am a react developer'));
   }
 }
 
